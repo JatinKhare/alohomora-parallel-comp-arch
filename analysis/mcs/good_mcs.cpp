2dd5e6064bad571190f0b64e04e6712c3da63daf
Jatin Khare <jatinkhare@Jatins-MacBook-Air.local>
2022-11-26 01:04:06 -0600

    mcs with backoff

--- analysis/mcs/good_mcs.cpp
+++ analysis/mcs/good_mcs.cpp
@@ -1,4 +1,5 @@
 #include <benchmark/benchmark.h>
+#include <emmintrin.h>
 #include <atomic>
 #include <cstdint>
 #include <iostream>
@@ -6,6 +7,13 @@
 #include <vector>
 #include <pthread.h>
 #include <stdlib.h>
+#include <sched.h>
+#include <condition_variable>
+#include <mutex>
+#include <random>
+#include "../include/header.h"
+#include <chrono>
+#include <time.h>
 using namespace std;
 
 #define LOOP_COUNT 100000
@@ -13,6 +21,42 @@ using namespace std;
 //#define NUM_THREADS 8
 
 
+using namespace std::chrono;
+
+#define COUNTER
+//#define STACK_LIST
+
+
+//#define TIME_ANALYSIS
+
+//#define ORIGINAL
+#define BLOCKING_LOCK
+//#define PAUSE_x86
+//#define SCHED_YIELD
+//#define ACTIVE_BACKOFF
+//#define EXP_BACKOFF
+//#define RANDOM_BACKOFF
+
+#ifdef EXP_BACKOFF
+	#define MIN_BACKOFF 4
+	#define MAX_BACKOFF 1024
+#endif
+//#define PAUSE_ARM
+#ifdef BLOCKING_LOCK
+	std::condition_variable cvar;
+	std::mutex Mutex;
+#endif
+std::atomic<bool> lock_flag{false};
+int val;
+#ifdef TIME_ANALYSIS
+	std::chrono::time_point<std::chrono::system_clock> start[256], end_time[256];
+	std::chrono::duration<double> elapsed_seconds[256], max_elapsed_seconds[256], max_time;
+#endif
+#ifdef RANDOM_BACKOFF
+	// Random number generator
+  std::uniform_int_distribution<int> dist;
+  std::mt19937 rng;
+#endif
 std::atomic<int> lock_flag1{0};
 std::atomic<int> lock_flag2{0};
 
@@ -20,16 +64,6 @@ typedef struct qnode {
   struct qnode *next;
   bool locked;
 } qnode;
-
-
-void print_ll(qnode * root){
-  int i = 0;
-  while(root!=NULL){
-    cout<<"("<<std::this_thread::get_id()<<")"<<root<<"("<<i<<") -> ";
-    root = root->next;
-    i++;
-  }
-}
 
 
 int val =0;
@@ -47,9 +81,28 @@ void lock (qnode **L, qnode *I) {
     if (predecessor!=NULL) {
         I->locked = true;
         predecessor->next = I;
-        while (I->locked)
-		;
+        while (I->locked){
+        #ifdef SCHED_YIELD
+          sched_yield();
+        #endif
+
+        #ifdef PAUSE_ARM
+          asm volatile("yield");
+        #endif
+
+        #ifdef PAUSE_x86
+          asm volatile("pause");
+        #endif
+
+        #ifdef BLOCKING_LOCK
+          cvar.wait(lock);
+        #endif
+
+        #ifdef ORIGINAL
+          cvar.wait(lock);
+        #endif
     }
+}
 };
 
 void unlock (qnode** L, qnode *I) {
@@ -61,24 +114,12 @@ void unlock (qnode** L, qnode *I) {
     while (!I->next){;} 
 
     I->next->locked = false;
-    
+    #ifdef BLOCKING_LOCK
+    cvar.notify_all();
+    //cvar.notify_one();
+    #endif
 };
 };
-
-
-/*void *lock_example(void *arg) {
-  //std::cout<<" Thread = "<<std::this_thread::get_id()<<"\n";
-  qnode* I = new qnode;
-  for(int i=0;i<LOOP_COUNT;i++) {
-    my_lock(&L, I);
-      val++;
-    my_unlock(&L, I);
-  }
-  free(I);
-
-  return NULL;
-}*/
-
 
 // Increment val once each time the lock is acquired
 void inc(mcs_lock &t, std::int64_t &val) {
@@ -127,18 +168,3 @@ BENCHMARK(mcs_lock_benchmark)
     ->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
-
-
-/*int main(){
-
-  L = NULL; 
-  pthread_t threads[NUM_THREADS];
-  for (int i = 0; i < NUM_THREADS; i++) {
-    pthread_create( &threads[i], NULL, &lock_example,NULL);
-  }
-  for(int i=0;i<NUM_THREADS;i++) {
-    pthread_join(threads[i],NULL);
-  }
-  std::cout<<"counter = "<<val<<"\n";
-
-}*/
