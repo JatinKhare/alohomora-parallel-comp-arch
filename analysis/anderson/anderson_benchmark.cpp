b6513e419026a74de582af8edae61ef5c374047b
shreyas ravishankar <sravisha@login1.ls6.tacc.utexas.edu>
2022-11-26 20:53:43 -0600

    anderson and fetch-add working

---
+++ analysis/anderson/anderson_benchmark.cpp
@@ -0,0 +1,352 @@
+#include <benchmark/benchmark.h>
+#include <atomic>
+#include <cstdint>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sched.h>
+#include <condition_variable>
+#include <mutex>
+#include "../include/header.h"
+#include <chrono>
+#include <time.h>
+<<<<<<< HEAD
+#include <emmintrin.h>
+#include <random>
+=======
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+using namespace std::chrono;
+
+#define COUNTER
+//#define STACK_LIST
+
+<<<<<<< HEAD
+#define CRITICAL_SECTION_SIZE 1
+#define LOOP_COUNT 100000
+
+#define TIME_ANALYSIS
+
+//#define ORIGINAL
+#define BLOCKING_LOCK
+//#define PAUSE_x86
+//#define SCHED_YIELD
+//#define PAUSE_ARM
+//#define ACTIVE_BACKOFF
+//#define EXP_BACKOFF
+//#define RANDOM_BACKOFF
+
+#ifdef BLOCKING_LOCK
+	std::condition_variable cvar;
+	std::mutex Mutex;
+#endif
+
+#ifdef EXP_BACKOFF
+	#define MIN_BACKOFF 4
+	#define MAX_BACKOFF 1024
+#endif
+
+#ifdef RANDOM_BACKOFF
+	// Random number generator
+  std::uniform_int_distribution<int> dist;
+  std::mt19937 rng;
+#endif
+
+#define CACHELINE_SIZE 64
+
+int val;
+
+#ifdef TIME_ANALYSIS
+	std::chrono::time_point<std::chrono::system_clock> start[256], end_time[256];
+	std::chrono::duration<double> elapsed_seconds[256], max_elapsed_seconds[256], max_time;
+#endif
+
+class AndersonSpinLock {
+
+	public:	
+void my_lock(){
+
+  #ifdef BLOCKING_LOCK
+  	std::unique_lock<std::mutex> lock(Mutex);
+  #endif
+
+  #ifdef RANDOM_BACKOFF
+    rng.seed(std::random_device()());
+    dist = std::uniform_int_distribution<int>(4, 1024);
+  #endif 
+
+  #ifdef EXP_BACKOFF
+  int backoff_iters = MIN_BACKOFF;
+  #endif 
+    const size_t index = (NextFreeIdx.fetch_add(1))%LockedFlags.size();
+    auto &flag = LockedFlags[index].first;
+    // Ensure overflow never happens
+    if (index == 0)
+        NextFreeIdx -= LockedFlags.size();
+    while (flag){
+          #ifdef SCHED_YIELD
+          	sched_yield();
+          #endif
+
+          #ifdef PAUSE_ARM
+          	asm volatile("yield");
+          #endif
+
+          #ifdef PAUSE_x86
+          	asm volatile("pause");
+          #endif
+
+          #ifdef BLOCKING_LOCK
+          	cvar.wait(lock);
+          #endif
+
+          #ifdef ORIGINAL
+            ;
+          #endif
+
+          #ifdef ACTIVE_BACKOFF
+            for (volatile int i = 0; i < 150; i += 1);
+          #endif
+
+          #ifdef EXP_BACKOFF
+
+          // Pause for some number of iterations
+          for (int i = 0; i < backoff_iters; i++) {
+  		      _mm_pause();
+          }
+          // Get the backoff iterations for next time
+          backoff_iters = std::min(backoff_iters << 1, MAX_BACKOFF);
+
+          #endif
+
+          #ifdef RANDOM_BACKOFF
+            int backoff_iters = dist(rng);
+            // Pause for some number of iterations
+            for (int i = 0; i < backoff_iters; i++)  _mm_pause();
+          #endif    
+    }
+    flag = true;
+=======
+#define CRITICAL_SECTION_SIZE 1000
+#define LOOP_COUNT 1 
+
+//#define TIME_ANALYSIS
+
+#define ORIGINAL
+//#define BLOCKING_LOCK
+//#define PAUSE_x86
+//#define SCHED_YIELD
+
+
+//#define PAUSE_ARM
+#ifdef BLOCKING_LOCK
+  std::condition_variable cvar;
+  std::mutex Mutex;
+#endif
+#ifdef TIME_ANALYSIS
+  std::chrono::time_point<std::chrono::system_clock> start[256], end_time[256];
+  std::chrono::duration<double> elapsed_seconds[256], max_elapsed_seconds[256], max_time;
+#endif
+
+std::atomic<std::uint16_t> line{0};
+volatile std::uint16_t serving{0};int val;
+
+void my_lock(){
+      #ifdef BLOCKING_LOCK
+        std::unique_lock<std::mutex> lock(Mutex);
+      #endif
+      auto place = line.fetch_add(1);
+
+      while (serving != place){
+        
+        #ifdef SCHED_YIELD
+          sched_yield();
+        #endif
+
+        #ifdef PAUSE_ARM
+          asm volatile("yield");
+        #endif
+
+        #ifdef PAUSE_x86
+          asm volatile("pause");
+        #endif
+
+        #ifdef BLOCKING_LOCK
+          cvar.wait(lock);
+        #endif
+
+      } 
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+
+}
+
+void my_unlock(){
+<<<<<<< HEAD
+  #ifdef BLOCKING_LOCK
+    std::unique_lock<std::mutex> lock(Mutex);
+  #endif	
+  const size_t idx = NextServingIdx.fetch_add(1);
+  LockedFlags[idx%LockedFlags.size()].first = false;
+  #ifdef BLOCKING_LOCK
+    cvar.notify_all();
+    //cvar.notify_one();
+  #endif
+}
+
+using PaddedFlag = std::pair<std::atomic_bool,uint8_t[CACHELINE_SIZE-sizeof(std::atomic_bool)]>;
+alignas(CACHELINE_SIZE) std::vector<PaddedFlag> LockedFlags;
+alignas(CACHELINE_SIZE) std::atomic_size_t      NextFreeIdx = {0};
+alignas(CACHELINE_SIZE) std::atomic_size_t      NextServingIdx = {1};
+
+AndersonSpinLock(size_t maxThreads=2*std::thread::hardware_concurrency()) :
+    LockedFlags(maxThreads)
+    {
+        for (auto &flag : LockedFlags)
+            flag.first = true;
+ 
+        LockedFlags[0].first = false;
+    }
+
+};
+void increase_counter()
+{
+    for(int j = 0; j<CRITICAL_SECTION_SIZE; j++){
+	val++;
+    }
+}
+
+void *lock_example(AndersonSpinLock *and_lock,int i) { 
+  for(int j=0;j<LOOP_COUNT;j++) {
+#ifdef TIME_ANALYSIS
+	start[i] = high_resolution_clock::now();
+#endif
+	and_lock->my_lock();
+#ifdef TIME_ANALYSIS
+	end_time[i] = high_resolution_clock::now();
+=======
+  asm volatile("" : : : "memory");
+  serving = serving + 1;
+
+#ifdef BLOCKING_LOCK
+  cvar.notify_all();
+  //cvar.notify_one();
+#endif
+
+}
+void increase_counter()
+{
+    for(int j = 0; j<CRITICAL_SECTION_SIZE; j++){
+  val++;
+    }
+}
+
+void *lock_example(int i) { 
+  for(int j=0;j<LOOP_COUNT;j++) {
+#ifdef TIME_ANALYSIS
+  start[i] = high_resolution_clock::now();
+#endif
+  my_lock();
+#ifdef TIME_ANALYSIS
+  end_time[i] = high_resolution_clock::now();
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+#endif
+#ifdef STACK_LIST
+    push_pop_func(j);
+#endif
+
+#ifdef COUNTER
+    increase_counter();
+#endif
+<<<<<<< HEAD
+	and_lock->my_unlock();
+#ifdef TIME_ANALYSIS
+	elapsed_seconds[i] = end_time[i] - start[i];
+	if(elapsed_seconds[i].count() > max_elapsed_seconds[i].count())
+		max_elapsed_seconds[i] = elapsed_seconds[i];
+=======
+  my_unlock();
+#ifdef TIME_ANALYSIS
+  elapsed_seconds[i] = end_time[i] - start[i];
+  if(elapsed_seconds[i].count() > max_elapsed_seconds[i].count())
+    max_elapsed_seconds[i] = elapsed_seconds[i];
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+#endif
+  }
+
+  return NULL;
+}
+
+// Small Benchmark (Google benchmark runnning mechanism)
+<<<<<<< HEAD
+static void anderson_benchmark(benchmark::State &s) {
+=======
+static void cas_benchmark(benchmark::State &s) {
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+  // Sweep over a range of threads
+  auto num_threads = s.range(0);
+
+  // Allocate a vector of threads
+  std::vector<std::thread> threads;
+  threads.reserve(num_threads);
+<<<<<<< HEAD
+  // Timing loop
+  for (auto _ : s) {
+	  val = 0;
+	  //LockedFlags.resize(num_threads); 
+    AndersonSpinLock *andlock_= new AndersonSpinLock(num_threads);
+	  reset_sum();
+    for (auto i = 0u; i < num_threads; i++) {
+      threads.emplace_back([&] { lock_example(andlock_,i); });
+=======
+
+  // Timing loop
+  for (auto _ : s) {
+    val = 0;
+    reset_sum();
+    for (auto i = 0u; i < num_threads; i++) {
+      threads.emplace_back([&] { lock_example(i); });
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+    }
+    // Join threads
+    for (auto &thread : threads) thread.join();
+    threads.clear();
+#ifdef TIME_ANALYSIS
+    for(int i = 0; i<256; i++){
+<<<<<<< HEAD
+	    if(max_elapsed_seconds[i].count()>max_time.count())
+		    max_time = max_elapsed_seconds[i];
+=======
+      if(max_elapsed_seconds[i].count()>max_time.count())
+        max_time = max_elapsed_seconds[i];
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+    }
+#endif
+#ifdef COUNTER
+    //cout<<"val = "<<val<<"\n";
+    assert(val == LOOP_COUNT*CRITICAL_SECTION_SIZE*num_threads);
+#endif
+
+#ifdef STACK_LIST
+    //cout<<"size_of_SL() = "<<size_of_SL()<<", LOOP_COUNT/2 = "<<LOOP_COUNT/2<<", num_threads = "<<num_threads<<"\n";
+    assert(size_of_SL() == num_threads*LOOP_COUNT/2);
+#endif
+    auto max_ = -1;
+
+    //cout<<"Worse starvation = "<<*max_element(starve_count.begin(), starve_count.end())<<"\n";
+  }
+#ifdef TIME_ANALYSIS
+    cout<<"Maximum wait time = "<<max_time.count()<<"\n";
+#endif
+}
+<<<<<<< HEAD
+BENCHMARK(anderson_benchmark)
+=======
+BENCHMARK(cas_benchmark)
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+    ->RangeMultiplier(2)
+    ->Range(1, std::thread::hardware_concurrency()*2)
+    ->UseRealTime()
+    ->Unit(benchmark::kMillisecond);
+
+BENCHMARK_MAIN();
---
+++ analysis/anderson/anderson_thread_affinity.cpp
@@ -0,0 +1,266 @@
+#include <benchmark/benchmark.h>
+#include <cassert>
+#include <thread>
+#include <cstdint>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <pthread.h>
+#include <stdlib.h>
+using namespace std;
+
+<<<<<<< HEAD
+#define NUM_THREADS 4
+#define CRITICAL_SECTION_SIZE 1
+#define LOOP_COUNT 100000
+#define CACHELINE_SIZE 64
+class Spinlock {
+
+public:
+=======
+#define CACHELINE_SIZE 64
+
+#define NUM_THREADS 4
+#define CRITICAL_SECTION_SIZE 1
+#define LOOP_COUNT 100000
+
+class Spinlock {
+ private:
+ 	std::atomic<std::uint16_t> line{0};
+ 	volatile std::uint16_t serving{0};
+ public:
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+    Spinlock(size_t maxThreads=std::thread::hardware_concurrency()) :
+        LockedFlags(maxThreads)
+    {
+        for (auto &flag : LockedFlags)
+            flag.first = true;
+ 
+        LockedFlags[0].first = false;
+    }
+<<<<<<< HEAD
+ 
+    void lock()
+    {   
+        const size_t index = (NextFreeIdx.fetch_add(1))%LockedFlags.size();
+        auto &flag = LockedFlags[index].first;
+ 
+        // Ensure overflow never happens
+        if (index == 0)
+            NextFreeIdx -= LockedFlags.size();
+ 
+        while (flag);
+ 
+        flag = true;
+    }
+ 
+    void unlock()
+    {
+        const size_t idx = NextServingIdx.fetch_add(1);
+        LockedFlags[idx%LockedFlags.size()].first = false;
+    }
+ 
+
+    using PaddedFlag = std::pair<std::atomic_bool,
+                                 uint8_t[CACHELINE_SIZE-sizeof(std::atomic_bool)]>;
+    static_assert(sizeof(PaddedFlag) == CACHELINE_SIZE, "");
+ 
+    alignas(CACHELINE_SIZE) std::vector<PaddedFlag> LockedFlags;
+    alignas(CACHELINE_SIZE) std::atomic_size_t      NextFreeIdx = {0};
+    alignas(CACHELINE_SIZE) std::atomic_size_t      NextServingIdx = {1};
+=======
+ void lock()
+ {  
+     const size_t index = (NextFreeIdx.fetch_add(1))%LockedFlags.size();
+     auto &flag = LockedFlags[index].first;
+ 
+     // Ensure overflow never happens
+     if (index == 0)
+         NextFreeIdx -= LockedFlags.size();
+ 
+     while (flag);
+ 
+     flag = true;
+ }
+ void unlock()
+ {
+     const size_t idx = NextServingIdx.fetch_add(1);
+     LockedFlags[idx%LockedFlags.size()].first = false;
+ }
+ using PaddedFlag = std::pair<std::atomic_bool,
+                              uint8_t[CACHELINE_SIZE-sizeof(std::atomic_bool)]>;
+ static_assert(sizeof(PaddedFlag) == CACHELINE_SIZE, "");
+ 
+ alignas(CACHELINE_SIZE) std::vector<PaddedFlag> LockedFlags;
+ alignas(CACHELINE_SIZE) std::atomic_size_t      NextFreeIdx = {0};
+ alignas(CACHELINE_SIZE) std::atomic_size_t      NextServingIdx = {1};
+ 
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+};
+
+
+// Increment val once each time the lock is acquired
+<<<<<<< HEAD
+void inc(Spinlock *s, std::int64_t &val) {
+	for (int i = 0; i < LOOP_COUNT; i++) {
+		s->lock();
+		for(int j = 0; j<CRITICAL_SECTION_SIZE; j++)
+			val++;
+		//val++;
+		s->unlock();
+=======
+void inc(Spinlock &s, std::int64_t &val) {
+	for (int i = 0; i < LOOP_COUNT; i++) {
+		s.lock();
+		for(int j = 0; j<CRITICAL_SECTION_SIZE; j++)
+			val++;
+		s.unlock();
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+	}
+}
+
+//  Aligned type
+//  Allows us to keep to atomics from sitting on the same cache line
+struct alignas(64) AlignedAtomic {
+	AlignedAtomic(int v) { val = v; }
+	std::int64_t val;
+};
+
+void os_scheduler() {
+<<<<<<< HEAD
+
+	AlignedAtomic a{0};
+	AlignedAtomic b{0};
+	Spinlock* s1;
+=======
+	int affinity_ = 0;
+
+	AlignedAtomic a{0};
+	AlignedAtomic b{0};
+	//cout<<"a = "<<&a.val<<", b = "<<&b.val<<"\n";
+	Spinlock s1;
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+
+	std::vector<std::thread> threads(NUM_THREADS);
+	for (unsigned i = 0; i < NUM_THREADS; i++) {
+	if(i%2){
+		std::thread t([&]() {inc(s1, a.val); });
+		threads.push_back(std::move(t));
+	}
+	else{
+		std::thread t([&]() {inc(s1, b.val); });
+		threads.push_back(std::move(t));
+	}
+	}
+	for (std::thread & th : threads)
+<<<<<<< HEAD
+    {
+        // If thread Object is Joinable then Join that thread.
+        if (th.joinable())
+            th.join();
+    }
+	assert(a.val== NUM_THREADS*LOOP_COUNT*CRITICAL_SECTION_SIZE/2);
+	assert(b.val== NUM_THREADS*LOOP_COUNT*CRITICAL_SECTION_SIZE/2);
+=======
+    	 {
+        // If thread Object is Joinable then Join that thread.
+        if (th.joinable())
+            th.join();
+    	}
+	assert(a.val == NUM_THREADS*LOOP_COUNT*CRITICAL_SECTION_SIZE/2);
+	assert(b.val == NUM_THREADS*LOOP_COUNT*CRITICAL_SECTION_SIZE/2);
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+}
+// Data sharing benchmark w/ OS scheduling
+static void osScheduling(benchmark::State& s) {
+	while (s.KeepRunning()) {
+		os_scheduler();
+	}
+}
+BENCHMARK(osScheduling)->UseRealTime()->Unit(benchmark::kMillisecond);
+
+void thread_affinity() {
+<<<<<<< HEAD
+         
+	AlignedAtomic a{0};
+	AlignedAtomic b{0};
+	Spinlock *s1= new Spinlock(NUM_THREADS);
+	int index[4] = {0, 0, 0, 1};//worse case
+	//int index[4] = {0, 1, 0, 1};//best case
+	//int index[4] = {30, 7, 31, 127};
+	//int index[8] = {0, 1, 30, 31, 66, 67, 94, 97}; //worst arrangement
+	//int index[8] = {0, 1, 0, 1, 0, 1, 0, 1};	//best arrangement
+=======
+	AlignedAtomic a{0};
+	AlignedAtomic b{0};
+	//cout<<"a = "<<&a.val<<", b = "<<&b.val<<"\n";
+	Spinlock s1;
+	int index[4] = {0, 1, 2, 3};//worse case
+	//int index[4] = {0, 1, 0, 1};//best case
+	//int index[4] = {30, 7, 31, 127};
+	//int index[8] = {0, 1, 30, 31, 66, 67, 94, 97}; //worst arrangement
+	//int index[8] = {0, 0, 1, 1, 2, 2, 3, 3};	//best arrangement
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+	//int index[8] = {0, 4, 1, 5, 2, 6, 3, 7};	//cache ping pong
+	std::vector<std::thread> threads(NUM_THREADS);
+	for (unsigned i = 0; i < NUM_THREADS; i++) {
+		cpu_set_t cpuset;
+		CPU_ZERO(&cpuset);
+<<<<<<< HEAD
+		//CPU_SET(index[i], &cpuset);
+		//CPU_SET(i%2, &cpuset);
+		if(i%2)
+			CPU_SET(0, &cpuset);
+		else
+			CPU_SET(1, &cpuset);
+		//	CPU_SET(i, &cpuset);
+=======
+		CPU_SET(index[i], &cpuset);
+		//CPU_SET(i%2, &cpuset);
+
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+		int rc = -1;
+		if(i%2){
+			std::thread t([&]() {inc(s1, a.val); });
+
+		rc = pthread_setaffinity_np(t.native_handle(), sizeof(cpu_set_t), &cpuset);
+		threads.push_back(std::move(t));
+		//threads.emplace_back([&] { inc(s1, a.val); });
+		}
+		else{
+			std::thread t([&]() {inc(s1, b.val); });
+		rc = pthread_setaffinity_np(t.native_handle(), sizeof(cpu_set_t), &cpuset);
+			threads.push_back(std::move(t));
+			//threads.emplace_back([&] { inc(s1, b.val); });
+		}
+		if (rc != 0) {
+			std::cerr << "Error calling pthread_setaffinity_np: " << rc << "\n";
+		}
+	}
+	for (std::thread & th : threads)
+    	 {
+        // If thread Object is Joinable then Join that thread.
+        if (th.joinable())
+            th.join();
+    	}
+<<<<<<< HEAD
+	assert(a.val== NUM_THREADS*LOOP_COUNT*CRITICAL_SECTION_SIZE/2);
+	assert(b.val== NUM_THREADS*LOOP_COUNT*CRITICAL_SECTION_SIZE/2);
+
+}
+=======
+	assert(a.val == NUM_THREADS*LOOP_COUNT*CRITICAL_SECTION_SIZE/2);
+	assert(b.val == NUM_THREADS*LOOP_COUNT*CRITICAL_SECTION_SIZE/2);
+
+}	 
+>>>>>>> 9b00b83339dea3d27c8fb1b324da9f1a9b83a181
+
+// Data sharing benchmark w/ manual scheduling
+static void threadAffinity(benchmark::State& s) {
+	while (s.KeepRunning()) {
+		thread_affinity();
+	}
+}
+BENCHMARK(threadAffinity)->UseRealTime()->Unit(benchmark::kMillisecond);
+
+BENCHMARK_MAIN();
